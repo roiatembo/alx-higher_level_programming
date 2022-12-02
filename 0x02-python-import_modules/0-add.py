948ab5934847a1d4458978a2fed866c23fc71174
Roia Langa Tembo <roia@kali.theitmogul.co.za>
2022-12-02 03:12:08 +0200

    importing

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,2 +1,8 @@
-from add_0 import add
-print(add(1,2))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the sum of 1 and 2"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
